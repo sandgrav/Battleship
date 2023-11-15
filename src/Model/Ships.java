cbe48335c8d24e0ed73d6be22a8a005c01eb7c29
sandgrav <morten@sandgrav.se>
2023-11-15 21:56:43 +0100

    Replaced classes Ships, Ship and Position

--- src/Model/Ships.java
+++ src/Model/Ships.java
@@ -2,70 +2,86 @@ package Model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /** Written by Morten Sandgrav **/
 
 public class Ships extends Board {
 
-    private List<Ship> shipsList;
+    private List<Ship> shipsList = new ArrayList<>();
 
+    //Amro
     public List<Ship> getShipsList(){
         return this.shipsList;
     }
 
     public boolean placeShip(int startX, int startY, int length, Direction direction) {
-        int tempX = startX;
-        int tempY = startY;
-        int tempLength = length;
+        Position start = new Position(startX, startY);
+        Position positionIncrement;
+        Position positionRight;
+        List<Position> positionsToCheck = new ArrayList<>();
+        Position[] ship;
 
         if (direction == Direction.HORISONTAL) {
-            if (tempX > 0) {
-                tempX--;
-                tempLength++;
-            }
-            if ((tempX + tempLength) < 9) {
-                tempLength++;
-            } else if ((tempX + tempLength) > 9) {
-                return false;
-            }
-            for (int i = 0; i < tempLength; i++) {
-                if (!(isEmpty(tempX + i, tempY - 1) && isEmpty(tempX + i, tempY) && isEmpty(tempX + i, tempY + 1))) {
-                    return false;
-                }
-            }
-            for (int i = 0; i < length; i++) {
-                board[startX + i][startY] = "S";
-            }
-            this.shipsList.add(new Ship(new int[]{startX,startY},length,direction));
+            positionIncrement = new Position(1, 0);
+            positionRight = new Position(0, 1);
         } else {
-            if (tempY > 0){
-                tempY--;
-                tempLength++;
-            }
-            if ((tempY + tempLength) < 9) {
-                tempLength++;
-            } else if ((tempY + tempLength) > 9) {
+            positionIncrement = new Position(0,1);
+            positionRight = new Position(-1, 0);
+        }
+
+        Position tempPosition = start.subtractPosition(positionIncrement);
+        positionsToCheck.add(tempPosition);
+        positionsToCheck.add(tempPosition.subtractPosition(positionRight));
+        positionsToCheck.add(tempPosition.addPosition(positionRight));
+        tempPosition = start;
+        for (int i = 0; i < length; i++) {
+            if (0 <= tempPosition.getX() && tempPosition.getX() <= 9 && 0 <= tempPosition.getY() && tempPosition.getY() <= 9 && board[tempPosition.getX()][tempPosition.getY()].isEmpty()) {
+                positionsToCheck.add(tempPosition.subtractPosition(positionRight));
+                positionsToCheck.add(tempPosition.addPosition(positionRight));
+                tempPosition = tempPosition.addPosition(positionIncrement);
+            } else {
                 return false;
             }
-            for (int i = 0; i < tempLength; i++) {
-                if (!(isEmpty(tempX - 1, tempY + i) && isEmpty(tempX, tempY + i) && isEmpty(tempX + 1, tempY + i))) {
+        }
+        positionsToCheck.add(tempPosition);
+        positionsToCheck.add(tempPosition.subtractPosition(positionRight));
+        positionsToCheck.add(tempPosition.addPosition(positionRight));
+
+        for (Position pos: positionsToCheck) {
+            if (0 <= pos.getX() && pos.getX() <= 9 && 0 <= pos.getY() && pos.getY() <= 9) {
+                if (!board[pos.getX()][pos.getY()].isEmpty()) {
                     return false;
                 }
             }
-            for (int i = 0; i < length; i++) {
-                board[startX][startY + i] = "S";
-            }
-            this.shipsList.add(new Ship(new int[]{startX,startY},length,direction));
         }
+
+        ship = new Position[length];
+        tempPosition = start;
+        for (int i = 0; i < length; i++) {
+            board[tempPosition.getX()][tempPosition.getY()] = "X";
+            ship[i] = tempPosition;
+            tempPosition = tempPosition.addPosition(positionIncrement);
+        }
+
+        // Amro
+        shipsList.add(new Ship(ship));
 
         return true;
     }
 
-    boolean isEmpty(int x, int y) {
-        if (x < 0 || x > 9 || y < 0 || y > 9)
-            return true;
-        else
-            return board[x][y].isEmpty();
+    public char checkForShip(Position position) {
+        for (Ship ship: shipsList) {
+            if (ship.checkForShip(position)) {
+                if (ship.isSunk()) {
+                    return 's';
+                } else {
+                    return 'h';
+                }
+            }
+        }
+
+        return 'm';
     }
 }
 
